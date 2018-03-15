fe38801a75be7a6d8d65f573e7be651bfec3f7e7
shashkovdanil <shashkovdanil@gmail.com>
2018-03-16 00:40:05 +0500

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const config = {
+  entry: ['babel-polyfill', './lib/renderers/dom.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'lib'), 'node_modules']
+  }
+};
+
+module.exports = config;

08cd021e9cc0bc20458c114f91324500b19e59fc
shashkovdanil <shashkovdanil@gmail.com>
2018-04-13 17:28:45 +0500

    Performance Optimization and Prod build

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,42 @@
 const path = require('path');
 
 const config = {
-  entry: ['babel-polyfill', './lib/renderers/dom.js'],
+  entry: {
+    vendor: ['babel-polyfill', 'react', 'react-dom', 'prop-types', 'axios', 'lodash.debounce', 'lodash.pickby'],
+    app: ['./lib/renderers/dom.js'],
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: '[name].js',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
-      }
-    ]
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'env', 'stage-2'],
+          }
+        },
+      },
+    ],
   },
   resolve: {
-    modules: [path.resolve(__dirname, 'lib'), 'node_modules']
-  }
+    modules: [path.resolve(__dirname, 'lib'), 'node_modules'],
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
+        },
+      },
+    },
+  },
 };
 
 module.exports = config;
